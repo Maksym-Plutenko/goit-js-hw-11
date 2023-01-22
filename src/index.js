650476379841f934408314afb3fe5745bd92951a
Maksym-Plutenko <plutenkom@gmail.com>
2023-01-22 14:37:16 +0200

    button logick

--- src/index.js
+++ src/index.js
@@ -1,78 +1,70 @@
-// console.log("Hello!");
-
-import Notiflix from 'notiflix';
 import axios from "axios";
+import Notiflix from 'notiflix';
 
 const input = document.querySelector('#search-form input');
 const button = document.querySelector('button');
 const gallery = document.querySelector('.gallery');
 
-console.log(input);
-console.log(button);
-console.log(gallery);
+// console.log(input);
+// console.log(button);
+// console.log(gallery);
 
-// async function pixabayGetAPI() {
-//     const KEY = '33025300-4f56a11ea42b0ad7a58370454';
-//     const response = await fetch(`https://pixabay.com/api/?key=${KEY}&q=yellow+flowers&image_type=photo`);
-//     return response.json();
-// }
+button.addEventListener('click', event => {
+    event.preventDefault();
 
-// const axios = require('axios');
-
-// axios.get('https://pixabay.com/api/?key=33025300-4f56a11ea42b0ad7a58370454&q=yellow+flowers&image_type=photo')
-//   .then(function (response) {
-//     console.log(response);
-//   })
-
-// async function pixabayGetAPI() {
-//     const KEY = '33025300-4f56a11ea42b0ad7a58370454';
-//     const response = await axios.get(`https://pixabay.com/api/?key=${KEY}&q=yellow+flowers&image_type=photo`);
-//     return response.json();
-// }
-
-// const resp = pixabayGetAPI();
-// console.log(resp);
-
-// axios.get('https://pixabay.com/api/?key=33025300-4f56a11ea42b0ad7a58370454&q=yellow+flowers&image_type=photo')
-//   .then(res => {
-//     console.log(res.data);
-//   });
+    if (input.value) {
+        pixabayAPI(input.value);
+        console.log(input.value)
+    }
+})
 
 async function pixabayAPI(request) {
     const KEY = '33025300-4f56a11ea42b0ad7a58370454';
     try {
         const response = await axios.get(`https://pixabay.com/api/?key=${KEY}&q=${request}&image_type=photo&orientation=horizontal&safesearch=true`);
-        console.log(response);
+        // console.log(response);
         const picturesArr = response.data.hits;
-        console.log(picturesArr);
-        console.log(picturesArr[0]);
+        // console.log(picturesArr);
+        // console.log(picturesArr[0]);
+
         let galleryMarkup = '';
-        galleryMarkup = picturesArr.forEach(element => {
-            galleryMarkup = galleryMarkup + `
-                <div class="photo-card">
-                    <img src="${element.webformatURL}" alt="" loading="lazy" />
-                    <div class="info">
-                        <p class="info-item">
-                            <b>Likes</b>
-                        </p>
-                        <p class="info-item">
-                            <b>Views</b>
-                        </p>
-                        <p class="info-item">
-                            <b>Comments</b>
-                        </p>
-                        <p class="info-item">
-                            <b>Downloads</b>
-                        </p>
+
+        if (picturesArr.length === 0) {
+            Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.");
+        } else {
+            picturesArr.forEach(element => {
+                galleryMarkup = galleryMarkup + `
+                    <div class="photo-card">
+                        <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" />
+                        <div class="info">
+                            <p class="info-item">
+                                <b>Likes </b>
+                                ${element.likes}
+                            </p>
+                            <p class="info-item">
+                                <b>Views </b>
+                                ${element.views}
+                            </p>
+                            <p class="info-item">
+                                <b>Comments </b>
+                                ${element.comments}
+                            </p>
+                            <p class="info-item">
+                                <b>Downloads </b>
+                                ${element.downloads}
+                            </p>
+                        </div>
                     </div>
-                </div>
-            `;
-            gallery.innerHTML = galleryMarkup;
-        });
+                `;
+            });
+        }
+        
+        gallery.innerHTML = galleryMarkup;
     } catch(error) {
-        console.log(error.message);
+        // console.log(error.message);
+        Notiflix.Notify.failure(error.message);
     }
 }
 
-pixabayAPI('yellow+flowers');
+// pixabayAPI('cat');
 
